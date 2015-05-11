5836c357835a752a84a2a1c9d6ab19b1b7461299
Rococolate <chocolatebloom@gmail.com>
2015-05-11 20:55:04 +0800

    testpage

--- online2/index.js
+++ online2/index.js
@@ -2,35 +2,43 @@
     var input = document.querySelector('input');
 
     input.onchange = function () {
-        // 也可以传入图片路径：lrz('../demo.jpg', ...
-        lrz(this.files[0], {width: 400}, function (results) {
-            // 你需要的数据都在这里，可以以字符串的形式传送base64给服务端转存为图片。
-            console.log(results);
-
-            // 以下为演示用内容
-            var tip = document.querySelector('#tip'),
-                report = document.querySelector('#report'),
-                footer = document.querySelector('footer');
-
-            report.innerHTML = footer.innerHTML =  '';
-            tip.innerHTML = '<p>正在生成和上传..</p> <small class="text-muted">演示未优化移动端内存占用，可能会造成几秒内卡顿或闪退，不代表真实表现，请亲测。</small>';
-            demo_report('原始图片', results.origin, results.origin.size);
-
-            setTimeout(function () {
-                demo_report('客户端预压的图片', results.base64, results.base64.length * 0.8);
-
-                // 发送到后端
-                
-            }, 100);
-        });
+        if(checkImg(this.files[0])){// 也可以传入图片路径：lrz('../demo.jpg', ...
+                lrz(this.files[0], {width: 400}, function (results) {
+                    // 你需要的数据都在这里，可以以字符串的形式传送base64给服务端转存为图片。
+                    console.log(results);
+        
+                    // 以下为演示用内容
+                    var tip = document.querySelector('#tip'),
+                        report = document.querySelector('#report'),
+                        footer = document.querySelector('footer');
+        
+                    report.innerHTML = footer.innerHTML =  '';
+                    tip.innerHTML = '<p>正在生成和上传..</p> <small class="text-muted">演示未优化移动端内存占用，可能会造成几秒内卡顿或闪退，不代表真实表现，请亲测。</small>';
+                    demo_report('原始图片', results.origin, results.origin.size);
+        
+                    setTimeout(function () {
+                        demo_report('客户端预压的图片', results.base64, results.base64.length * 0.8);
+        
+                        // 发送到后端
+                        
+                    }, 100);
+                });}
     };
-
+    function checkImg(file){
+        if(/\.(gif|jpg|jpeg|png|GIF|JPG|PNG)$/.test(file.value)){
+            return true;
+        }else{ 
+            alert("请上传gif|jpg|jpeg|png|GIF|JPG|PNG文件")
+            return false
+        }
+    }
     /**
      * 演示报告
      * @param title
      * @param src
      * @param size
      */
+
     function demo_report(title, src, size) {
         var img = new Image(),
             li = document.createElement('li'),
