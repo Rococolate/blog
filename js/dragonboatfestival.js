1eeff0aee606c17c52bf7e962d37b7faa6641a1b
Rococolate <chocolatebloom@gmail.com>
2016-03-17 22:48:55 +0800

    测试上传

--- js/dragonboatfestival.js
+++ js/dragonboatfestival.js
@@ -519,10 +519,10 @@ window.onload = function(){
 	indexlock ++;
 }
 
-var indextime = setInterval(function(){
+// var indextime = setInterval(function(){
 
-	if(indexlock >= 1){
-		$loading.css("display","none"); 
-		clearInterval(indextime);
-	}
-},2000);
+// 	if(indexlock >= 1){
+// 		// $loading.css("display","none"); 
+// 		clearInterval(indextime);
+// 	}
+// },2000);
